d47a0705ee4f3ca19a48da7242fb8c4fa8b78391
Bhushan kakade <kakadebhushan467@gmail.com>
2024-06-24 08:13:57 +0530

    create project setup install bootsrap call get patient rest api ,send reat Api in angular create admin dashboard call get appointment rest api create appointment form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdmindashComponent } from './admindash/admindash.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { CreateAppointmentComponent } from './create-appointment/create-appointment.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdmindashComponent,
+    AppointmentComponent,
+    CreateAppointmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
